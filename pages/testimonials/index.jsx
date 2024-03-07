514d90c736d375c4728754cfb135a68984d8dbc5
P A Chathura Madhava <chathuratechdev@gmail.com>
2024-03-07 18:35:45 +0530

    Apply Testimonials Component

---
+++ pages/testimonials/index.jsx
@@ -0,0 +1,36 @@
+// components
+import TestimonialSlider from "../../components/TestimonialSlider";
+
+// framer motion
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
+
+const Testimonials = () => {
+  return (
+    <div className="h-full py-32 text-center bg-primary/30">
+      <div className="container flex flex-col justify-center h-full mx-auto">
+        {/* title */}
+        <motion.h2
+          variants={fadeIn("up", 0.2)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          className="mb-8 h2 xl:mb-0"
+        >
+          What clients <span className="text-accent">say.</span>
+        </motion.h2>
+        {/* slider */}
+        <motion.div
+          variants={fadeIn("up", 0.4)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+        >
+          <TestimonialSlider />
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonials;
